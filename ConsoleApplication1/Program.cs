fe77a0142e4213f5a50a17f93f7d2863d19a536c
Oleg <legogon@mail.ru>
2015-11-07 18:34:58 +0300

    Класс Лист

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,11 +10,23 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(1, 1, '*');
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
+            Point p2 = new Point(1, 2, '*');
             p2.Draw();
+
+            Point p3 = new Point(1, 3, '*');
+            p3.Draw();
+
+            Point p4 = new Point(1, 4, '#');
+            p4.Draw();
+
+            List<Point> numList = new List<Point>();
+            numList.Add(p1);
+            numList.Add(p2);
+            numList.Add(p3);
+            numList.Add(p4);
 
             Console.ReadLine();
         }
